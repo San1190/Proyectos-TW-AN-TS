e2dbd234869e9b85e2c05ee3526c79471e3e1597
san1190 <sanfuenal@gmail.com>
2024-05-31 20:33:17 +0200

    c46

--- Angular/CursoAngular/bases/src/app/heroes/hero/hero.component.ts
+++ Angular/CursoAngular/bases/src/app/heroes/hero/hero.component.ts
@@ -9,6 +9,16 @@ export class HeroComponent {
 
   public name: string = 'ironman';
   public age: number = 45;
+
+
+
+  get capitalizedName():string{
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription(): string{
+    return `${this.name} - ${this.age}`
+  }
   
 
 
