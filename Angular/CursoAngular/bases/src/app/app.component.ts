165e0b4d342757dfa76105fabe35277586df1630
san1190 <sanfuenal@gmail.com>
2024-05-31 16:30:05 +0200

    clase 43

---
+++ Angular/CursoAngular/bases/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  public title: string = 'Hola mundo';
+  public counter: number = 10;
+
+  increaseBy(value : number): void {
+    this.counter += value;
+
+
+  }
+
+  default(): void{
+    this.counter = 10;
+  }
+
+
+}
