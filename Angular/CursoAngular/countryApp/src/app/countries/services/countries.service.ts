20d27fe36ce1869403341e36a5b3c7550aada9f6
San1190 <sanfuenal@gmail.com>
2024-06-18 00:23:50 +0200

    126

--- Angular/CursoAngular/countryApp/src/app/countries/services/countries.service.ts
+++ Angular/CursoAngular/countryApp/src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, map, of, tap } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({providedIn: 'root'})
@@ -13,8 +13,11 @@ export class CountriesService {
     searchCapital(term: string): Observable<Country[]>{
 
         return this.http.get<Country[]>(`${this.apiUrl}/capital/${term}`)
+          .pipe(
+            catchError(error => of([])),
+          );
 
 
     }
-    
+
 }
