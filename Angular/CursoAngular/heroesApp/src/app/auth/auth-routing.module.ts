da6f2d99d48ef55793f501eb819a516dacd9d5a3
San1190 <sanfuenal@gmail.com>
2024-07-08 18:56:33 +0200

    185

--- Angular/CursoAngular/heroesApp/src/app/auth/auth-routing.module.ts
+++ Angular/CursoAngular/heroesApp/src/app/auth/auth-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 
 
-const routes: Routes = [];
+
+//localhost:4200/auth/
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutPageComponent,
+    
+
+
+  },
+];
 
 
 @NgModule({
